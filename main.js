40d092c39db40777c3a35b7e631dea6712dd07aa
Hammed Oyedele <itz.harmid@gmail.com>
2021-01-10 11:35:20 +0100

    feat(main.js): add support for new items
    - foreign keys
    - index keys
    - unique keys
    - primary keys (composite and simple)
    - comments
    - default values
    - nullable values
    - enumerations (remains generating classes)

--- main.js
+++ main.js
@@ -3,139 +3,243 @@ const path = require('path')
 const { noCase, pascalCase } = require('change-case')
 const CodeWriter = require('./code-writer')
 
-function generateCode (diagram, folder) {
-  diagram.ownedViews
-    .filter(view => view instanceof type.UMLClassView)
-    .map(classView => classView.model)
-    .filter(umlClass => umlClass.stereotype === 'Table')
-    .forEach(table => {
-      const date = new Date()
-      const writer = new CodeWriter()
-      const timestampColumns = ['created_at', 'updated_at']
-      const typesWithMultiplicity = [
-        'char',
-        'time',
-        'float',
-        'double',
-        'timeTz',
-        'string',
-        'decimal',
-        'dateTime',
-        'dateTimeTz',
-        'softDeletes',
-        'softDeletesTz',
-        'unsignedDecimal'
-      ]
-      const databaseTableName = noCase(table.name, {
-        transform: (part, index, parts) =>
-          `${part}${parts.length - 1 === index ? '' : '_'}`.toLowerCase()
-      })
-        .split(' ')
-        .join('')
+function sanitizeTableName (name) {
+  return noCase(name, {
+    transform: (part, index, parts) =>
+      `${part}${parts.length - 1 === index ? '' : '_'}`.toLowerCase()
+  })
+    .split(' ')
+    .join('')
+}
 
-      writer.writeLines([
-        '<?php',
-        '',
-        'use Illuminate\\Support\\Facades\\Schema;',
-        'use Illuminate\\Database\\Schema\\Blueprint;',
-        'use Illuminate\\Database\\Migrations\\Migration;',
-        '',
-        `class Create${pascalCase(table.name)}Table extends Migration`,
-        '{'
-      ])
+function getViews (diagram, type) {
+  return diagram.ownedViews
+    .filter(view => view instanceof type)
+    .map(typeView => typeView.model)
+}
 
-      writer.indent()
+function generateMigrations (diagram, folder) {
+  const tables = getViews(diagram, type.UMLClassView).filter(
+    umlClass => umlClass.stereotype === 'Table'
+  )
+  const enumerations = getViews(diagram, type.UMLEnumerationView)
 
-      writer.writeLines([
-        '/**',
-        ' * Run the migrations.',
-        ' *',
-        ' * @return void',
-        ' */',
-        'public function up()',
-        '{'
-      ])
+  if (!tables.length) {
+    return app.toast.error('There is no migrations to generate!')
+  }
 
-      writer.indent()
+  tables.forEach(table => {
+    const date = new Date()
+    const writer = new CodeWriter()
+    const ids = table.attributes
+      .filter(attribute => attribute.isID)
+      .map(attribute => `'${attribute.name}'`)
+    const usedEnums = table.attributes
+      .filter(attribute => attribute.type instanceof type.UMLEnumeration)
+      .map(attribute => attribute.type.name)
+    const timestampColumns = ['created_at', 'updated_at']
+    const typesWithMultiplicity = [
+      'set',
+      'char',
+      'time',
+      'float',
+      'double',
+      'timeTz',
+      'string',
+      'decimal',
+      'dateTime',
+      'dateTimeTz',
+      'softDeletes',
+      'softDeletesTz',
+      'unsignedDecimal'
+    ]
+    const databaseTableName = sanitizeTableName(table.name)
 
-      writer.writeLine(
-        `Schema::create('${databaseTableName}', function (Blueprint $table) {`
+    writer.writeLines(
+      ['<?php', ''].concat(
+        usedEnums.map(usedEnum => `use App\\Enums\\${usedEnum};`),
+        [
+          'use Illuminate\\Support\\Facades\\Schema;',
+          'use Illuminate\\Database\\Schema\\Blueprint;',
+          'use Illuminate\\Database\\Migrations\\Migration;',
+          '',
+          `class Create${pascalCase(table.name)}Table extends Migration`,
+          '{'
+        ]
       )
+    )
 
-      writer.indent()
+    writer.indent()
 
-      table.attributes
-        .filter(attribute => timestampColumns.indexOf(attribute.name) === -1)
-        .forEach(({ name, type, multiplicity }) => {
+    writer.writeLines([
+      '/**',
+      ' * Run the migrations.',
+      ' *',
+      ' * @return void',
+      ' */',
+      'public function up()',
+      '{'
+    ])
+
+    writer.indent()
+
+    writer.writeLine(
+      `Schema::create('${databaseTableName}', function (Blueprint $table) {`
+    )
+
+    writer.indent()
+
+    table.attributes
+      .filter(attribute => timestampColumns.indexOf(attribute.name) === -1)
+      .forEach(
+        ({
+          name,
+          isID,
+          isUnique,
+          stereotype,
+          multiplicity,
+          defaultValue,
+          documentation,
+          type: dataType
+        }) => {
           writer.writeLine('$table->')
 
-          if (typeof type === 'string') {
-            writer.write(`${type}('${name}'`)
+          if (typeof dataType === 'string') {
+            writer.write(`${dataType}('${name}'`)
 
             if (
-              typesWithMultiplicity.indexOf(type) !== -1 &&
+              typesWithMultiplicity.indexOf(dataType) !== -1 &&
               multiplicity !== ''
             ) {
               writer.write(', ' + multiplicity.split('..').join(', '))
             }
+          } else if (dataType instanceof type.UMLEnumeration) {
+            writer.write(`enum('${name}', ${dataType.name}::values()`)
+          }
 
-            writer.write(')')
+          writer.write(')')
+
+          if (isUnique) {
+            writer.write('->unique()')
+          }
+
+          if (defaultValue !== '') {
+            if (defaultValue.toLowerCase() === 'null') {
+              writer.write('->nullable()')
+            } else {
+              writer.write(
+                `->default(${
+                  dataType instanceof type.UMLEnumeration
+                    ? `${dataType.name}::${defaultValue}`
+                    : defaultValue
+                })`
+              )
+            }
+          }
+
+          // if we have multiple ids, we will handle it later...
+          if (isID && ids.length <= 1) {
+            writer.write('->primary()')
+          }
+
+          if (documentation !== '') {
+            writer.write(`->comment('${documentation.replace("'", "\\'")}')`)
           }
 
           writer.write(';')
-        })
 
-      if (
-        table.attributes.some(
-          attribute => timestampColumns.indexOf(attribute.name) !== -1
-        )
-      ) {
-        writer.writeLine('$table->timestamps();')
-      }
+          if (stereotype !== null) {
+            const stereotypeLower = stereotype.toLowerCase()
 
-      writer.outdent()
+            if (stereotypeLower === 'in' || stereotypeLower === 'index') {
+              return writer.writeLine(`$table->index('${name}');`)
+            }
 
-      writer.writeLine('});')
+            if (stereotypeLower.indexOf('fk') === 0) {
+              const foreignDefs = stereotype.split(':')
 
-      writer.outdent()
+              if (foreignDefs.length >= 2) {
+                writer.writeLine(`$table->foreign('${name}')`)
 
-      writer.writeLines([
-        '}',
-        '',
-        '/**',
-        ' * Reverse the migrations.',
-        ' *',
-        ' * @return void',
-        ' */',
-        'public function down()',
-        '{'
-      ])
+                writer.indent()
 
-      writer.indent()
+                writer.writeLines([
+                  `->references('${foreignDefs[2] || 'id'}')`,
+                  `->on('${sanitizeTableName(foreignDefs[1])}')`,
+                  `->onUpdate('${foreignDefs[3] || 'cascade'}')`,
+                  `->onDelete('${foreignDefs[4] || 'cascade'}');`
+                ])
 
-      writer.writeLine(`Schema::dropIfExists('${databaseTableName}');`)
-
-      writer.outdent()
-
-      writer.writeLine('}')
-
-      writer.outdent()
-
-      writer.writeLines(['}', ''])
-
-      // Laravel Migrations file format: <year>_<month>_<day>_<hour><minute><second>_create_<table>_table.php
-      fs.writeFileSync(
-        path.join(
-          folder,
-          `${date.getFullYear()}_${date.getMonth() +
-            1}_${date.getDate()}_${date.getHours()}${date.getMinutes()}${date.getSeconds()}_create_${databaseTableName}_table.php`
-        ),
-        writer.getData()
+                writer.outdent()
+              }
+            }
+          }
+        }
       )
-    })
+
+    // handle composite keys
+    if (ids.length > 1) {
+      writer.writeLine(`$table->primary([${ids.join(', ')}]);`)
+    }
+
+    // handle timestamps
+    if (
+      table.attributes.some(
+        attribute => timestampColumns.indexOf(attribute.name) !== -1
+      )
+    ) {
+      writer.writeLine('$table->timestamps();')
+    }
+
+    writer.outdent()
+
+    writer.writeLine('});')
+
+    writer.outdent()
+
+    writer.writeLines([
+      '}',
+      '',
+      '/**',
+      ' * Reverse the migrations.',
+      ' *',
+      ' * @return void',
+      ' */',
+      'public function down()',
+      '{'
+    ])
+
+    writer.indent()
+
+    writer.writeLine(`Schema::dropIfExists('${databaseTableName}');`)
+
+    writer.outdent()
+
+    writer.writeLine('}')
+
+    writer.outdent()
+
+    writer.writeLines(['}', ''])
+
+    // Laravel Migrations file format: <year>_<month>_<day>_<hour><minute><second>_create_<table>_table.php
+    fs.writeFileSync(
+      path.join(
+        folder,
+        `${date.getFullYear()}_${date.getMonth() +
+          1}_${date.getDate()}_${date.getHours()}${date.getMinutes()}${date.getSeconds()}_create_${databaseTableName}_table.php`
+      ),
+      writer.getData()
+    )
+  })
+
+  enumerations.forEach(enumeration => {
+    const writer = new CodeWriter()
+  })
+
+  app.toast.info(`${tables.length} migrations generated successfully.`)
 }
 
-function getOutputFolderAndGenerateCode (diagram) {
+function getOutputFolderAndGenerateMigrations (diagram) {
   const files = app.dialogs.showOpenDialog(
     'Select a folder where generated codes to be located',
     null,
@@ -144,7 +248,7 @@ function getOutputFolderAndGenerateCode (diagram) {
   )
 
   if (files && files.length > 0) {
-    generateCode(diagram, files[0])
+    generateMigrations(diagram, files[0])
   }
 }
 
@@ -163,16 +267,16 @@ function handleGenerateCommand (diagram, folder) {
 
           // If folder is not assigned, popup Open Dialog to select a folder
           if (!folder) {
-            getOutputFolderAndGenerateCode(diagram)
+            getOutputFolderAndGenerateMigrations(diagram)
           } else {
-            generateCode(diagram, folder)
+            generateMigrations(diagram, folder)
           }
         }
       })
   } else if (!folder) {
-    getOutputFolderAndGenerateCode(diagram)
+    getOutputFolderAndGenerateMigrations(diagram)
   } else {
-    generateCode(diagram, folder)
+    generateMigrations(diagram, folder)
   }
 }
 
