479d8c73dfdc2f58fd5b050ef0936ee034c9b13c
Hammed Oyedele <itz.harmid@gmail.com>
2021-01-10 06:57:52 +0100

    feat(main): support basic code generation

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const { noCase } = require('change-case')
+const { noCase, pascalCase } = require('change-case')
 const CodeWriter = require('./code-writer')
 
 function generateCode (diagram, folder) {
@@ -11,19 +11,124 @@ function generateCode (diagram, folder) {
     .forEach(table => {
       const date = new Date()
       const writer = new CodeWriter()
-      const tableName = noCase(table.name, {
+      const timestampColumns = ['created_at', 'updated_at']
+      const typesWithMultiplicity = [
+        'char',
+        'time',
+        'float',
+        'double',
+        'timeTz',
+        'string',
+        'decimal',
+        'dateTime',
+        'dateTimeTz',
+        'softDeletes',
+        'softDeletesTz',
+        'unsignedDecimal'
+      ]
+      const databaseTableName = noCase(table.name, {
         transform: (part, index, parts) =>
           `${part}${parts.length - 1 === index ? '' : '_'}`.toLowerCase()
       })
+        .split(' ')
+        .join('')
 
-      // do something badass here!
+      writer.writeLines([
+        '<?php',
+        '',
+        'use Illuminate\\Support\\Facades\\Schema;',
+        'use Illuminate\\Database\\Schema\\Blueprint;',
+        'use Illuminate\\Database\\Migrations\\Migration;',
+        '',
+        `class Create${pascalCase(table.name)}Table extends Migration`,
+        '{'
+      ])
+
+      writer.indent()
+
+      writer.writeLines([
+        '/**',
+        ' * Run the migrations.',
+        ' *',
+        ' * @return void',
+        ' */',
+        'public function up()',
+        '{'
+      ])
+
+      writer.indent()
+
+      writer.writeLine(
+        `Schema::create('${databaseTableName}', function (Blueprint $table) {`
+      )
+
+      writer.indent()
+
+      table.attributes
+        .filter(attribute => timestampColumns.indexOf(attribute.name) === -1)
+        .forEach(({ name, type, multiplicity }) => {
+          writer.writeLine('$table->')
+
+          if (typeof type === 'string') {
+            writer.write(`${type}('${name}'`)
+
+            if (
+              typesWithMultiplicity.indexOf(type) !== -1 &&
+              multiplicity !== ''
+            ) {
+              writer.write(', ' + multiplicity.split('..').join(', '))
+            }
+
+            writer.write(')')
+          }
+
+          writer.write(';')
+        })
+
+      if (
+        table.attributes.some(
+          attribute => timestampColumns.indexOf(attribute.name) !== -1
+        )
+      ) {
+        writer.writeLine('$table->timestamps();')
+      }
+
+      writer.outdent()
+
+      writer.writeLine('});')
+
+      writer.outdent()
+
+      writer.writeLines([
+        '}',
+        '',
+        '/**',
+        ' * Reverse the migrations.',
+        ' *',
+        ' * @return void',
+        ' */',
+        'public function down()',
+        '{'
+      ])
+
+      writer.indent()
+
+      writer.writeLine(`Schema::dropIfExists('${databaseTableName}');`)
+
+      writer.outdent()
+
+      writer.writeLine('}')
+
+      writer.outdent()
+
+      writer.writeLines(['}', ''])
 
       // Laravel Migrations file format: <year>_<month>_<day>_<hour><minute><second>_create_<table>_table.php
       fs.writeFileSync(
         path.join(
           folder,
           `${date.getFullYear()}_${date.getMonth() +
-            1}_${date.getDate()}_${date.getHours()}${date.getMinutes()}${date.getSeconds()}_create_${tableName}_table.php`
+            1}_${date.getDate()}_${date.getHours()}${date.getMinutes()}${date.getSeconds()}_create_${databaseTableName}_table.php`
         ),
         writer.getData()
       )
