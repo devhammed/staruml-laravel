78c631208ed7e2125b3a34461ac4c72459188c1d
Hammed Oyedele <itz.harmid@gmail.com>
2021-02-18 10:28:49 +0100

    feat: add better foreign keys support using associations

--- main.js
+++ main.js
@@ -22,277 +22,259 @@ function sanitizeTableName (name) {
 }
 
 /**
- * Generate Laravel Migrations Timestamp.
- *
- * It will add 2 seconds to timestamp if `isForeign` is true (to maintain order).
- *
- * @param {boolean} isForeign
- * @returns {string} year_month_day_hourMinuteSecond
- */
-function getMigrationTimestamp (isForeign = false) {
-  const date = new Date()
-  const seconds = isForeign ? date.getSeconds() + 2 : date.getSeconds()
-
-  return `${date.getFullYear()}_${date.getMonth() +
-    1}_${date.getDate()}_${date.getHours()}${date.getMinutes()}${seconds}`
-}
-
-/**
- * Check if UML class is a table.
- *
- * @param {type.UMLClass} umlClass
- * @returns {boolean}
- */
-function isClassATable (umlClass) {
-  return umlClass.stereotype.toLowerCase() === 'table'
-}
-
-/**
  * Get model views from diagram.
  *
  * @param {type.UMLClassDiagram} diagram
  * @param {type.UMLView} type
- * @returns {any[]}
+ * @returns {type.UMLObject[]}
  */
 function getViews (diagram, type) {
   return diagram.ownedViews
     .filter(view => view instanceof type)
-    .map(view => view.model)
+    .map(typeView => typeView.model)
+}
+
+/**
+ * Get associated classes.
+ *
+ * @param {type.UMLClass} umlClass
+ * @returns {type.UMLAssociation[]}
+ */
+function getClassAssociations (umlClass) {
+  return umlClass.ownedElements.filter(
+    element => element instanceof type.UMLAssociation
+  )
 }
 
 function generateMigrations (diagram, folder) {
-  const tables = getViews(diagram, type.UMLClassView).filter(isClassATable)
-  const enumerations = getViews(diagram, type.UMLEnumerationView)
-  const associations = getViews(diagram, type.UMLAssociationView).filter(
-    association =>
-      isClassATable(association.end1.reference) &&
-      isClassATable(association.end2.reference)
+  const tables = getViews(diagram, type.UMLClassView).filter(
+    umlClass => umlClass.stereotype.toLowerCase() === 'table'
   )
+  const enumerations = getViews(diagram, type.UMLEnumerationView)
 
   if (!tables.length) {
     return app.toast.error('There is no migrations to generate!')
   }
 
-  tables.forEach(table => {
-    const writer = new CodeWriter()
-    const ids = table.attributes
-      .filter(attribute => attribute.isID)
-      .map(attribute => `'${attribute.name}'`)
-    const usedEnums = table.attributes
-      .filter(attribute => attribute.type instanceof type.UMLEnumeration)
-      .map(attribute => attribute.type.name)
-    const timestampColumns = ['created_at', 'updated_at']
-    const typesWithMultiplicity = [
-      'set',
-      'char',
-      'time',
-      'float',
-      'double',
-      'timeTz',
-      'string',
-      'decimal',
-      'dateTime',
-      'dateTimeTz',
-      'softDeletes',
-      'softDeletesTz',
-      'unsignedDecimal'
-    ]
-    const databaseTableName = sanitizeTableName(table.name)
+  tables
+    .sort((a, b) => {
+      if (getClassAssociations(b).some(dep => dep.end2.reference === a)) {
+        return -1
+      }
 
-    writer.migration(
-      `Create${pascalCase(table.name)}Table`,
-      usedEnums.map(usedEnum => `use App\\Enums\\${usedEnum};`),
-      writer => {
-        writer.writeLine(
-          `Schema::create('${databaseTableName}', function (Blueprint $table) {`
+      if (getClassAssociations(a).some(dep => dep.end2.reference === b)) {
+        return 1
+      }
+
+      return 0
+    })
+    .forEach((table, tableIndex) => {
+      const date = new Date()
+      const writer = new CodeWriter()
+      const ids = table.attributes
+        .filter(attribute => attribute.isID)
+        .map(attribute => `'${attribute.name}'`)
+      const usedEnums = table.attributes
+        .filter(attribute => attribute.type instanceof type.UMLEnumeration)
+        .map(attribute => attribute.type.name)
+      const timestampColumns = ['created_at', 'updated_at']
+      const typesWithMultiplicity = [
+        'set',
+        'char',
+        'time',
+        'float',
+        'double',
+        'timeTz',
+        'string',
+        'decimal',
+        'dateTime',
+        'dateTimeTz',
+        'softDeletes',
+        'softDeletesTz',
+        'unsignedDecimal'
+      ]
+      const databaseTableName = sanitizeTableName(table.name)
+
+      writer.writeLines(
+        ['<?php', ''].concat(
+          usedEnums.map(usedEnum => `use App\\Enums\\${usedEnum};`),
+          [
+            'use Illuminate\\Support\\Facades\\Schema;',
+            'use Illuminate\\Database\\Schema\\Blueprint;',
+            'use Illuminate\\Database\\Migrations\\Migration;',
+            '',
+            `class Create${pascalCase(table.name)}Table extends Migration`,
+            '{'
+          ]
         )
+      )
+
+      writer.indent()
+
+      writer.writeLines([
+        '/**',
+        ' * Run the migrations.',
+        ' *',
+        ' * @return void',
+        ' */',
+        'public function up()',
+        '{'
+      ])
+
+      writer.indent()
+
+      writer.writeLine(
+        `Schema::create('${databaseTableName}', function (Blueprint $table) {`
+      )
+
+      writer.indent()
+
+      table.attributes
+        .filter(attribute => timestampColumns.indexOf(attribute.name) === -1)
+        .forEach(
+          ({
+            name,
+            isID,
+            isUnique,
+            stereotype,
+            multiplicity,
+            defaultValue,
+            documentation,
+            type: dataType
+          }) => {
+            writer.writeLine('$table->')
+
+            if (
+              typeof dataType === 'string' &&
+              columnTypes.indexOf(dataType) !== -1
+            ) {
+              writer.write(`${dataType}('${name}'`)
+
+              if (
+                typesWithMultiplicity.indexOf(dataType) !== -1 &&
+                multiplicity !== ''
+              ) {
+                writer.write(', ' + multiplicity.split('..').join(', '))
+              }
+            } else if (dataType instanceof type.UMLEnumeration) {
+              writer.write(`enum('${name}', ${dataType.name}::values()`)
+            } else {
+              // I don't know what this person is thinking, let's just do `text` abeg...
+              writer.write(`text('${name}'`)
+            }
+
+            writer.write(')')
+
+            if (isUnique) {
+              writer.write('->unique()')
+            }
+
+            if (defaultValue !== '') {
+              if (defaultValue.toLowerCase() === 'null') {
+                writer.write('->nullable()')
+              } else {
+                writer.write(
+                  `->default(${
+                    dataType instanceof type.UMLEnumeration
+                      ? `${dataType.name}::${defaultValue}`
+                      : defaultValue
+                  })`
+                )
+              }
+            }
+
+            // if we have multiple ids, we will handle it later...
+            if (isID && ids.length <= 1) {
+              writer.write('->primary()')
+            }
+
+            if (documentation !== '') {
+              writer.write(`->comment('${documentation.replace("'", "\\'")}')`)
+            }
+
+            writer.write(';')
+
+            if ((stereotype = (stereotype || '').toLowerCase())) {
+              if (stereotype === 'in' || stereotype === 'index') {
+                return writer.writeLine(`$table->index('${name}');`)
+              }
+            }
+          }
+        )
+
+      // handle composite keys
+      if (ids.length > 1) {
+        writer.writeLine(`$table->primary([${ids.join(', ')}]);`)
+      }
+
+      // handle timestamps
+      if (
+        table.attributes.some(
+          attribute => timestampColumns.indexOf(attribute.name) !== -1
+        )
+      ) {
+        writer.writeLine('$table->timestamps();')
+      }
+
+      getClassAssociations(table).forEach(({ end1, end2 }, index, arr) => {
+        writer.writeLine(`$table->foreign('${end1.name}')`)
 
         writer.indent()
 
-        table.attributes
-          .filter(attribute => timestampColumns.indexOf(attribute.name) === -1)
-          .forEach(
-            ({
-              name,
-              isID,
-              isUnique,
-              stereotype,
-              multiplicity,
-              defaultValue,
-              documentation,
-              type: dataType
-            }) => {
-              writer.writeLine('$table->')
-
-              if (
-                typeof dataType === 'string' &&
-                columnTypes.indexOf(dataType) !== -1
-              ) {
-                writer.write(`${dataType}('${name}'`)
-
-                if (
-                  typesWithMultiplicity.indexOf(dataType) !== -1 &&
-                  multiplicity !== ''
-                ) {
-                  writer.write(', ' + multiplicity.split('..').join(', '))
-                }
-              } else if (dataType instanceof type.UMLEnumeration) {
-                writer.write(`enum('${name}', ${dataType.name}::values()`)
-              } else {
-                // I don't know what this person is thinking, let's just do `text` abeg...
-                writer.write(`text('${name}'`)
-              }
-
-              writer.write(')')
-
-              if (isUnique) {
-                writer.write('->unique()')
-              }
-
-              if (defaultValue !== '') {
-                if (defaultValue.toLowerCase() === 'null') {
-                  writer.write('->nullable()')
-                } else {
-                  writer.write(
-                    `->default(${
-                      dataType instanceof type.UMLEnumeration
-                        ? `${dataType.name}::${defaultValue}`
-                        : defaultValue
-                    })`
-                  )
-                }
-              }
-
-              // if we have multiple ids, we will handle it later...
-              if (isID && ids.length <= 1) {
-                writer.write('->primary()')
-              }
-
-              if (documentation !== '') {
-                writer.write(
-                  `->comment('${documentation.replace("'", "\\'")}')`
-                )
-              }
-
-              writer.write(';')
-
-              if (stereotype !== null) {
-                const stereotypeLower = stereotype.toLowerCase()
-
-                if (stereotypeLower === 'in' || stereotypeLower === 'index') {
-                  return writer.writeLine(`$table->index('${name}');`)
-                }
-              }
-            }
-          )
-
-        // handle composite keys
-        if (ids.length > 1) {
-          writer.writeLine(`$table->primary([${ids.join(', ')}]);`)
-        }
-
-        // handle timestamps
-        if (
-          table.attributes.some(
-            attribute => timestampColumns.indexOf(attribute.name) !== -1
-          )
-        ) {
-          writer.writeLine('$table->timestamps();')
-        }
+        writer.writeLines([
+          `->references('${end2.name}')`,
+          `->on('${sanitizeTableName(end2.reference.name)}')`,
+          `->onDelete('cascade');`
+        ])
 
         writer.outdent()
 
-        writer.writeLine('});')
-      },
-      writer => {
-        writer.writeLine(`Schema::dropIfExists('${databaseTableName}');`)
-      }
-    )
-
-    // Laravel Migrations file format: <timestamp>_create_<table>_table.php
-    fs.writeFileSync(
-      path.join(
-        folder,
-        `${getMigrationTimestamp()}_create_${databaseTableName}_table.php`
-      ),
-      writer.getData()
-    )
-  })
-
-  if (associations.length > 0) {
-    const writer = new CodeWriter()
-    const tables = associations.reduce((acc, { end1, end2 }) => {
-      const table = sanitizeTableName(end1.reference.name)
-
-      acc[table] = (acc[table] || []).concat({
-        field: end1.name,
-        references: end2.name,
-        on: sanitizeTableName(end2.reference.name)
+        if (index !== arr.length - 1) {
+          writer.writeLine('')
+        }
       })
 
-      return acc
-    }, {})
-    const tablesNames = Object.keys(tables)
+      writer.outdent()
 
-    writer.migration(
-      'AddForeignKeysToTables',
-      [],
-      writer => {
-        tablesNames.forEach((tableName, index) => {
-          writer.tableModification(tableName, writer => {
-            const foreignKeys = tables[tableName]
+      writer.writeLine('});')
 
-            foreignKeys.forEach(({ field, on, references }, i) => {
-              writer.writeLine(`$table->foreign('${field}')`)
+      writer.outdent()
 
-              writer.indent()
+      writer.writeLines([
+        '}',
+        '',
+        '/**',
+        ' * Reverse the migrations.',
+        ' *',
+        ' * @return void',
+        ' */',
+        'public function down()',
+        '{'
+      ])
 
-              writer.writeLines([
-                `->references('${references}')`,
-                `->on('${on}')`,
-                `->onDelete('cascade');`
-              ])
+      writer.indent()
 
-              writer.outdent()
+      writer.writeLine(`Schema::dropIfExists('${databaseTableName}');`)
 
-              writer.addBlankLineIfNotLast(i, foreignKeys)
-            })
-          })
+      writer.outdent()
 
-          writer.addBlankLineIfNotLast(index, tablesNames)
-        })
-      },
-      writer => {
-        tablesNames.forEach((tableName, index) => {
-          writer.tableModification(tableName, writer => {
-            const foreignKeysToDrop = tables[tableName]
-              .reduce((acc, { field }) => {
-                const fieldString = `'${field}'`
+      writer.writeLine('}')
 
-                if (acc.indexOf(fieldString) === -1) {
-                  acc.push(fieldString)
-                }
+      writer.outdent()
 
-                return acc
-              }, [])
-              .join(', ')
+      writer.writeLines(['}', ''])
 
-            writer.writeLine(`$table->dropForeign([${foreignKeysToDrop}]);`)
-          })
-
-          writer.addBlankLineIfNotLast(index, tablesNames)
-        })
-      }
-    )
-
-    fs.writeFileSync(
-      path.join(
-        folder,
-        `${getMigrationTimestamp(true)}_add_foreign_keys_to_tables.php`
-      ),
-      writer.getData()
-    )
-  }
+      // Laravel Migrations file format: <year>_<month>_<day>_<hour><minute><second>_create_<table>_table.php
+      fs.writeFileSync(
+        path.join(
+          folder,
+          `${date.getFullYear()}_${date.getMonth() +
+            1}_${date.getDate()}_${date.getHours()}${date.getMinutes()}${date.getSeconds() +
+            tableIndex}_create_${databaseTableName}_table.php`
+        ),
+        writer.getData()
+      )
+    })
 
   if (enumerations.length > 0) {
     const enumsDir = path.join(folder, 'Enums')
@@ -325,7 +307,10 @@ function generateMigrations (diagram, folder) {
 
         writer.writeLine(`public const ${name} = '${name}';`)
 
-        writer.addBlankLineIfNotLast(index, literals)
+        // if not last item, add a blank line after definition...
+        if (index !== literals.length - 1) {
+          writer.writeLine('')
+        }
       })
 
       writer.outdent()
@@ -340,7 +325,7 @@ function generateMigrations (diagram, folder) {
   }
 
   app.toast.info(
-    `${tables.length} migrations, ${enumerations.length} enums and ${associations.length} associations generated successfully.`
+    `${tables.length} migrations and ${enumerations.length} enums generated successfully.`
   )
 }
 
@@ -380,6 +365,6 @@ function handleLaravelGenerateCommand (diagram, folder) {
   }
 }
 
-exports.init = function () {
+exports.init = function init () {
   app.commands.register('laravel:generate', handleLaravelGenerateCommand)
 }
